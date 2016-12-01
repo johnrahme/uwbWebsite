282adf050f30a2e4d32a0ead79aa32cea9859c9e
John Rahme <john.rahme.se@gmail.com>
2016-12-01 16:49:39 +0100

    COnnection to db and positioning working

--- index.php
+++ index.php
@@ -9,10 +9,15 @@
      <div class="container clear-top" style="box-shadow: 0px 0px 5px 2px #888888; background-color: #fff; padding: 18px">
      <div class="jumbotron">
         <h1>Welcome to the uwb website!</h1>
-        <p><?php printFlights(); ?></p>
+        <p id = "currentPos"></p>
         <p>Do something fun by pressing the button below: </p>
          <a href = "<?php echo $baseDir; ?>/search"><button class = "btn btn-primary">Fun stuff</button></a>
       </div>
+         <div id="imageContainer" align = "middle">
+             <div id="dot"></div>
+             <img id="image" align="middle" src="https://s-media-cache-ak0.pinimg.com/564x/d9/cd/60/d9cd60c5a7687ff39fb226b55f1dbb1f.jpg" class="img-responsive" alt="Cinque Terre">
+         </div>
+    </div>
 
        
 <!-- INSERT BODY HTML HERE END-->
@@ -21,7 +26,14 @@
 	<script src = "<?php echo $rootPath.'js/custom/updatePositions.js';?>"></script>
 	
 	<script>
-		getPosition("test", '<?php echo $rootPath.'PHP/Positions/latest.php';?>');
+
+        setInterval(updatePosition, 100);
+
+
+        function updatePosition() {
+            getPosition("test", '<?php echo $rootPath.'PHP/Positions/latest.php';?>');
+        }
+
 	</script
 
 <!-- INSERT SCRIPTS HERE HERE END-->
--- js/custom/updatePositions.js
+++ js/custom/updatePositions.js
@@ -9,12 +9,42 @@ var data = new FormData();
             cache: false,
             contentType: false,
             processData: false,
-            success: function (response) {
-               
-                alert(response);
-            },
+            success: updateImage,
             error: function (e) {
                 alert(e.responseText);
             }
 			});
+}
+
+function updateImage(response){
+    //alert(response);
+    var dataArr = response.split(" ");
+    var xIn = parseFloat(dataArr[1]);
+    var yIn = parseFloat(dataArr[3]);
+
+    var color = '#000000';
+    var size = '20px';
+    var image = $("#image");
+    var dx = image.position().left;
+    var dy = image.position().top;
+
+    var width = image.width();
+    var height = image.height();
+
+    var roomWidth = 400;
+    var roomHeight = 400;
+
+    var pixelPerMeterX = width/roomWidth;
+    var pixelPerMeterY = height/roomHeight;
+
+    var x = dx+xIn*pixelPerMeterX;
+    var y = dy+yIn*pixelPerMeterY;
+    //$("#currentPos").html(x+" : "+y);
+    $("#dot")
+            .css('position', 'absolute')
+            .css('top', y + 'px')
+            .css('left', x + 'px')
+            .css('width', size)
+            .css('height', size)
+            .css('background-color', color);
 }
