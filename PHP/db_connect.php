5a12d935f3adda89c86dabddc3bb25bddb41dae7
John Rahme <john.rahme.se@gmail.com>
2016-12-08 18:24:37 +0100

    Positions updating correctly and live db added

--- PHP/db_connect.php
+++ PHP/db_connect.php
@@ -2,7 +2,10 @@
 
 //Server
 //$link = mysqli_connect("rerun","potiro","pcXZb(kL", "poti");
-$link = mysqli_connect("localhost","root","", "uwb");
+//$link = mysqli_connect("localhost","root","", "uwb");
+
+//Server connection
+$link = mysqli_connect("mysql525.loopia.se","john@f160124","6EAUbs6sdCYb", "futf_se_db_2");
 
 if (mysqli_connect_errno())
   {
--- js/custom/updatePositions.js
+++ js/custom/updatePositions.js
@@ -19,8 +19,10 @@ var data = new FormData();
 function updateImage(response){
     //alert(response);
     var dataArr = response.split(" ");
-    var xIn = parseFloat(dataArr[1]);
-    var yIn = parseFloat(dataArr[3]);
+    var yIn = parseFloat(dataArr[1]);
+    var xIn = parseFloat(dataArr[3]);
+    var roomWidth = 2.5;
+    var roomHeight = 1.4;
 
     var color = '#000000';
     var size = '20px';
@@ -31,15 +33,13 @@ function updateImage(response){
     var width = image.width();
     var height = image.height();
 
-    var roomWidth = 400;
-    var roomHeight = 400;
-
     var pixelPerMeterX = width/roomWidth;
     var pixelPerMeterY = height/roomHeight;
 
+    //Invert x and y coordinates
     var x = dx+xIn*pixelPerMeterX;
     var y = dy+yIn*pixelPerMeterY;
-    //$("#currentPos").html(x+" : "+y);
+    $("#currentPos").html(dx+" : "+dy);
     $("#dot")
             .css('position', 'absolute')
             .css('top', y + 'px')
